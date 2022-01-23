423bb1f0c650ca7da6186ada97633e62c300ba21
R4ndro <randyivanorrego@gmail.com>
2022-01-22 19:10:57 -0600

    zp top

---
+++ top_zp.php
@@ -0,0 +1,46 @@
+<?php
+
+use Traits\Sql\Top15;
+
+require './traits/sql/top_zp.php';
+    $sql = new Top15();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TOP PLAYERS</title>
+
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center text-white mb-4 mt-4">TOP PLAYERS</h2>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <thead>
+                            <tr class="success">
+                                <th class="text-center">#</th>
+                                <th>Avatar</th>
+                                <th>Nick</th>
+                                <th>Nivel</th>
+                                <th>Reset</th>
+                                <th>Experiencia</th>
+                            </tr>
+                        </thead>
+                        <tbody id="player-table">
+                            <?php $sql->__fnShowTop15( ); ?>
+                        </tbody>
+                        
+                    
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
